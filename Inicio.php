f6f74a23f18b185215684137cbdb5870c59d4a51
Oscar Marin <oscmarinr@gmail.com>
2021-03-13 13:00:50 -0400

    prueba 002

--- Inicio.php
+++ Inicio.php
@@ -3,14 +3,18 @@
 	//	A CONTINUACION COLOCAR 'SI' PARA PONER LA PAGINA EN MANTENIMIENTO  //		
 	//---------------------------------------------------------------------//
 
-	$_SESSION['EN_MANT'] = 'NO';
+	// $_SESSION['EN_MANT'] = 'NO';
+
+	// if ($_SESSION["EN_MANT"] == "NO") {
+	// 	$_contenedor_principal = "_inicio.php";
+	// } else {    
+	// 	$_contenedor_principal = "_en_mant.php";
+	// }
 
 	$_ruta1	= "";
-	$_ruta2	= "";    
-	if ($_SESSION["EN_MANT"] == "NO") {
-		$_contenedor_principal = "_inicio.php";
-	} else {    
-		$_contenedor_principal = "_en_mant.php";
-	}
+	$_ruta2	= ""; 
+
+	$_contenedor_principal = "_inicio.php";
 	include $_ruta1.'_plantill/_plantilla_00.php';
+
 ?>
